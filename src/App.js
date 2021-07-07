b7e59ebb27726b54b47321658d08318c2f000526
Phil Smithies <philsmithies@gmail.com>
2021-07-07 12:48:28 +0200

    set state;

--- src/App.js
+++ src/App.js
@@ -5,11 +5,74 @@ import './App.css';
 
 const App = () => {
 
+  const [state, setState]  = useState({
+    fullName: '',
+    role: '',
+    email: '',
+    location: '',
+    bio: '',
+    addWorkExperience: false
+  })
+
+  const handleChange = (e) => {
+   const value = e.target.value;
+    setState({
+      ...state,
+      [e.target.name]: value
+    })
+  }
+
+  const addWork = (e) => {
+    setState({
+      ...state,
+      addWorkExperience: true
+    })
+    console.log(state.addWorkExperience)
+  }
+
+
   return (
     <div className="App">
+      <h1>CV Generator</h1>
+       <form>
+        <input type="text" 
+        name="fullName" 
+        placeholder="John Smith" 
+        onChange={handleChange}
+        />
+        <input type="text" 
+        name="role" 
+        placeholder="Software Developer" 
+        onChange={handleChange}
+        />
+        <input type="text" 
+        name="email" 
+        placeholder="Email" 
+        onChange={handleChange}
+        />
+        <input type="text" 
+        name="location" 
+        placeholder="London, UK" 
+        onChange={handleChange}
+        />
+        <input type="text" 
+        name="bio" 
+        height="300px"
+        placeholder="Cupidatat sunt anim incididunt nisi labore sunt nulla Lorem elit irure. Aliquip quis excepteur et nostrud enim irure nostrud officia. Et deserunt et aliquip voluptate elit cupidatat. Adipisicing enim minim do anim eiusmod est. Irure laboris anim voluptate proident. Cillum reprehenderit est magna minim. Nostrud ex aute laborum ea irure amet ea ipsum ut non minim anim nisi." 
+        onChange={handleChange}
+        />
+      </form>
       <h1>Personal Details:</h1>
-      <PersonalDetails />
+      <PersonalDetails 
+        fullName={state.fullName}
+        role={state.role}
+        email={state.email}
+        location={state.location}
+        bio={state.bio}
+      />
       <h1>Work Experience</h1>
+      {state.addWorkExperience === false ? <button onClick={addWork}>Add Work</button> : ""} 
+      {state.addWorkExperience === true ? "Adding the work in the form" : ""} 
       <WorkExperience />
     </div>
   );
--- src/Components/PersonalDetails.js
+++ src/Components/PersonalDetails.js
@@ -1,57 +1,11 @@
-import React, { useState } from 'react';
-
-const PersonalDetails = () => {
-  const [state, setState]  = useState({
-    fullName: '',
-    role: '',
-    email: '',
-    location: '',
-    bio: '',
-  })
-
-  const handleChange = (e) => {
-   const value = e.target.value;
-    setState({
-      ...state,
-      [e.target.name]: value
-    })
-  }
-
+const PersonalDetails = (props) => {
   return(
     <div>
-    <form>
-        <input type="text" 
-        name="fullName" 
-        placeholder="John Smith" 
-        onChange={handleChange}
-        />
-        <input type="text" 
-        name="role" 
-        placeholder="Software Developer" 
-        onChange={handleChange}
-        />
-        <input type="text" 
-        name="email" 
-        placeholder="Email" 
-        onChange={handleChange}
-        />
-        <input type="text" 
-        name="location" 
-        placeholder="London, UK" 
-        onChange={handleChange}
-        />
-        <input type="text" 
-        name="bio" 
-        height="300px"
-        placeholder="Cupidatat sunt anim incididunt nisi labore sunt nulla Lorem elit irure. Aliquip quis excepteur et nostrud enim irure nostrud officia. Et deserunt et aliquip voluptate elit cupidatat. Adipisicing enim minim do anim eiusmod est. Irure laboris anim voluptate proident. Cillum reprehenderit est magna minim. Nostrud ex aute laborum ea irure amet ea ipsum ut non minim anim nisi." 
-        onChange={handleChange}
-        />
-    </form>
-    <div>
-    <br/>
-    <p>Hey your full name name is:{state.fullName}</p>
-    <p>Hey your role is:{state.role}</p>
-    </div>
+      <p>Full name:{props.fullName}</p>
+      <p>Role:{props.role}</p>
+      <p>Email:{props.email}</p>
+      <p>Location:{props.location}</p>
+      <p>Bio:{props.bio}</p>
     </div>
   ) 
 }
